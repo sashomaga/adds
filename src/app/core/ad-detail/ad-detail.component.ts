a9152fb27d9a06c3e1c8dbd94aa0ffaa629a310f
Alex <saswork@hotmail.com>
2016-05-23 21:50:58 +0300

    app structure

---
+++ src/app/core/ad-detail/ad-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-ad-detail',
+  templateUrl: 'ad-detail.component.html',
+  styleUrls: ['ad-detail.component.css']
+})
+export class AdDetailComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
